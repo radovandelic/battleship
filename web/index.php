a910cbcd759e4ee78498efa24e626ea41d724056
Radovan Delic <infamouszgbgd@yahoo.com>
2018-01-22 14:36:06 +0100

    visits

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-require('../vendor/autoload.php');
+require '../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
@@ -8,36 +8,36 @@ $app['debug'] = true;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-  array(
-   'pdo.server' => array(
-   'driver'   => 'pgsql',
-   'user' => $dbopts["user"],
-   'password' => $dbopts["pass"],
-   'host' => $dbopts["host"],
-   'port' => $dbopts["port"],
-   'dbname' => ltrim($dbopts["path"],'/')
-  )
- )
+    array(
+        'pdo.server' => array(
+            'driver' => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"], '/'),
+        ),
+    )
 );
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__ . '/views',
 ));
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig');
 });
 
-$app->get('/read/', function() use($app) {
+$app->get('/read/', function () use ($app) {
     $id = intval($_GET['id']);
     $query = "SELECT * from gamedata WHERE id = $id;";
     $st = $app['pdo']->prepare($query);
@@ -49,23 +49,23 @@ $app->get('/read/', function() use($app) {
         $row['gamestate'] = json_decode($row['gamestate']);
         $data = json_encode($row);
     }
-    
+
     $response = new Response();
     $response->setContent($data);
     $response->setStatusCode(Response::HTTP_OK);
-    
+
     // set a HTTP response header
     $response->headers->set('Content-Type', 'text/html');
-    
+
     // print the HTTP headers followed by the content
     $response->send();
 });
 
-$app->get('/write/', function() use($app) {
+$app->get('/write/', function () use ($app) {
     $column = $_GET['column'];
     $id = intval($_GET['id']);
     $value = $_GET['value'];
-    if(!is_numeric($value)) { $value = "'$value'"; }
+    if (!is_numeric($value)) {$value = "'$value'";}
 
     $query = "UPDATE gamedata SET $column = $value WHERE id = $id;";
 
@@ -75,15 +75,15 @@ $app->get('/write/', function() use($app) {
     $response = new Response();
     $response->setContent("New record created successfully.");
     $response->setStatusCode(Response::HTTP_OK);
-    
+
     // set a HTTP response header
     $response->headers->set('Content-Type', 'text/html');
-    
+
     // print the HTTP headers followed by the content
     $response->send();
 });
 
-$app->get('/reset/', function() use($app) {
+$app->get('/reset/', function () use ($app) {
     $init = "[
       [null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null],
@@ -104,17 +104,22 @@ $app->get('/reset/', function() use($app) {
     $response = new Response();
     $response->setContent("Server succesfully reset.");
     $response->setStatusCode(Response::HTTP_OK);
-    
+
     // set a HTTP response header
     $response->headers->set('Content-Type', 'text/html');
-    
+
     // print the HTTP headers followed by the content
     $response->send();
 });
 
- $app->post('/visit/', function() use($app) {
+$app->post('/visit/', function () use ($app) {
 
-    $content = $this->get("request")->getContent();
+    // the URI being requested (e.g. /about) minus any query parameters
+    $content = $request->getPathInfo();
+
+    // retrieve $_GET and $_POST variables respectively
+    //$content = $request->request->get('body');
+
     /*$location = $_POST['location'];
     $page = $_POST['page'];
     $browser = $_POST['browser'];
@@ -129,26 +134,26 @@ $app->get('/reset/', function() use($app) {
     $response = new Response();
     $response->setContent($content);
     $response->setStatusCode(Response::HTTP_OK);
-    
+
     // set a HTTP response header
     $response->headers->set('Content-Type', 'text/html');
-    
+
     // print the HTTP headers followed by the content
     $response->send();
 });
 
 /* $app->get('/visitors/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT * FROM visitors;');
-  $st->execute();
-  $visitors = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['location']);
-    $visitors[] = $row;
-  }
+$st = $app['pdo']->prepare('SELECT * FROM visitors;');
+$st->execute();
+$visitors = array();
+while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+$app['monolog']->addDebug('Row ' . $row['location']);
+$visitors[] = $row;
+}
 
-  return $app['twig']->render('visitors.twig', array(
-    'visitors' => $visitors
-  ));
+return $app['twig']->render('visitors.twig', array(
+'visitors' => $visitors
+));
 }); */
 
 $app->run();
